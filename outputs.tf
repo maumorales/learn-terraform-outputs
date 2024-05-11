93dbb404e362c55266a94146696d5b22c7e70f46
maumorales <mau.morales@gmail.com>
2024-05-11 13:54:09 -0600

    add outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@
 # SPDX-License-Identifier: MPL-2.0
 
 # Output declarations
+
+output "vpc_id" {
+  description = "ID of project VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "lb_url" {
+  description = "URL of load balancer"
+  value       = "http://${module.elb_http.elb_dns_name}/"
+}
+
+output "web_server_count" {
+  description = "Number of web servers provisioned"
+  value       = length(module.ec2_instances.instance_ids)
+}
